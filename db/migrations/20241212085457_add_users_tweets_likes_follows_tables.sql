ea4fced40f47b71e59d4a39df47088f27be237fa
Fikri Ranjabi <71055612+ranjabi@users.noreply.github.com>
2024-12-13 10:43:53 +0700

    feat: init db tables

---
+++ db/migrations/20241212085457_add_users_tweets_likes_follows_tables.sql
@@ -0,0 +1,41 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users (
+	id 				int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	username		varchar(16) NOT NULL,
+	email 			varchar(255) NOT NULL,
+	password 		varchar(128) NOT NULL,
+	created_at 		timestamp NOT NULL,
+	following_count int DEFAULT 0,
+	followers_count	int DEFAULT 0
+);
+
+CREATE TABLE tweets (
+	id 			int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	content		text NOT NULL,
+	created_at 	timestamp NOT NULL,
+	modified_at	timestamp,
+	user_id 	int NOT NULL REFERENCES users(id)
+);
+
+CREATE TABLE likes (
+	id 			int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	user_id		int NOT NULL REFERENCES users(id),
+	tweet_id	int NOT NULL REFERENCES tweets(id)
+);
+
+CREATE TABLE follows (
+	id				int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	following_id	int NOT NULL REFERENCES users(id),
+	followers_id	int NOT NULL REFERENCES users(id)
+);
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE follows;
+DROP TABLE likes;
+DROP TABLE tweets;
+DROP TABLE users;
+-- +goose StatementEnd
