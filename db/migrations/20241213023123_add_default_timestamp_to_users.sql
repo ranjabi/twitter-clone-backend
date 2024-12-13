1000a2360b0f945c27a4641d0c95101f15063a7f
Fikri Ranjabi <71055612+ranjabi@users.noreply.github.com>
2024-12-13 10:44:08 +0700

    fix: use default timestamp for users table

---
+++ db/migrations/20241213023123_add_default_timestamp_to_users.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE users
+ALTER COLUMN created_at SET DEFAULT CURRENT_TIMESTAMP;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE users
+ALTER COLUMN created_at DROP DEFAULT;
+-- +goose StatementEnd
