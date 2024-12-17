cba961566a6e4a9b7186099281c6eaccf1454196
Fikri Ranjabi <71055612+ranjabi@users.noreply.github.com>
2024-12-17 19:30:35 +0700

    fix: handle nil error in ServiceError

--- middleware/app_mux.go
+++ middleware/app_mux.go
@@ -22,17 +22,14 @@ const (
 
 type AppHandler func(http.ResponseWriter, *http.Request) *models.AppError
 
-// The ServeHTTP method called by the appHandler function and displays the returned error
+// todo: confirm this by looking at error trace when err is nul at ServiceError <--- The ServeHTTP method called by the appHandler function and displays the returned error
 func (fn AppHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if e := fn(w, r); e != nil {
 		// goes to logging
 		fmt.Println(utils.ColorLog(strconv.Itoa(e.Code), RED), utils.ColorLog(http.StatusText(e.Code), RED))
-		fmt.Println(utils.ColorLog("Message:", RED), utils.ColorLog(e.Message, RED))
-
-		if e.Error != nil {
-			errError := e.Error.Error()
-			fmt.Println(utils.ColorLog("Error:", RED), errError)
-		}
+		fmt.Println(utils.ColorLog(e.Error(), RED))
+		fmt.Printf("LOG: e: %#v\n", e)
+		fmt.Printf("LOG: e.Err: %#v\n", e.Err)
 
 		res, err := json.Marshal(models.ErrorResponse{Message: e.Message})
 		if err != nil {
--- models/app_error.go
+++ models/app_error.go
@@ -1,7 +1,42 @@
 package models
 
+// 	type AppError struct {
+// 	Err   error = 	type ServiceError struct {
+// 						Err   error  	// contain error type from repo or nil, want to preserve repo error to display on log
+// 						Message string	<---|	// business logic error
+// 					}						|
+// 	Message string -------------------------|
+// 	Code    int
+// 	}
+
 type AppError struct {
-	Error 	error
-	Message	string
-	Code	int
+	Err     error
+	Message string
+	Code    int
+}
+
+func (e AppError) Error() string {
+	// todo: pakai pointer!!!! kenapa??
+	if _, ok := e.Err.(*ServiceError); ok {
+		// fmt.Println("LOG: e.Err is type of ServiceError")
+		return e.Err.Error()
+	}
+
+	// fmt.Println("LOG: e.Err is type of built in error")
+	return e.Message + ": " + e.Err.Error()
+}
+
+type ServiceError struct {
+	Err     error
+	Message string
+}
+
+func (e ServiceError) Error() string {
+	// Err bisa nil ketika business logic service benar <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+	if e.Err != nil {
+		// fmt.Println("LOG: Service error .Err NOT NIL")
+		return e.Message + ": " + e.Err.Error()
+	}
+	// fmt.Println("LOG:  error .Err NIL")
+	return e.Message + ": No underlying error"
 }
