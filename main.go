286014cbdd8ef9abcb3be9aa446d4b8e3eb9d3c3
Fikri Ranjabi <71055612+ranjabi@users.noreply.github.com>
2024-12-18 22:00:45 +0700

    feat: apply migrations and seed at startup

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
+	"github.com/pressly/goose/v3"
+	_ "github.com/jackc/pgx/v5/stdlib"
 
 	"twitter-clone-backend/db"
 	"twitter-clone-backend/healthCheck"
@@ -16,8 +21,44 @@ import (
 	"twitter-clone-backend/utils"
 )
 
+const ENV = ".env.development"
+
+func applyMigrationsAndSeed(ctx context.Context) {
+	fmt.Println("Applying migrations and seed...")
+
+	db, err := sql.Open("pgx", utils.GetDbConnectionUrlFromEnv())
+	if err != nil {
+		log.Fatal("Error opening database connection:", err)
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	migrationsPath := filepath.Join(cwd, "db", "migrations")
+	seedPath := filepath.Join(cwd, "db", "seed")
+
+	fmt.Println("Starting migration reset...")
+	if err := goose.RunWithOptionsContext(ctx, "reset", db, migrationsPath, []string{}); err != nil {
+		log.Fatal("Migration reset failed:", err)
+	}
+
+	fmt.Println("Starting migration up...")
+	if err := goose.RunWithOptionsContext(ctx, "up", db, migrationsPath, []string{}); err != nil {
+		log.Fatal("Migration up failed:", err)
+	}
+
+	fmt.Println("Starting seed up...")
+	if err := goose.RunWithOptionsContext(ctx, "up", db, seedPath, []string{}, goose.WithNoVersioning()); err != nil {
+		log.Fatal("Seed up failed:", err)
+	}
+
+	fmt.Println("Migrations has been applied!")
+}
+
 func main() {
-	err := godotenv.Load(".env.development")
+	err := godotenv.Load(ENV)
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
@@ -29,6 +70,10 @@ func main() {
 		log.Fatal("Error getting database connection:", err)
 	}
 	defer db.CloseConnection()
+
+	if ENV != ".env.development" {
+		applyMigrationsAndSeed(ctx)
+	}
 
 	mux := new(middleware.AppMux)
 	mux.RegisterMiddleware(middleware.JwtAuthorization)
